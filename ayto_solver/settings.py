fb7e2f1352f7930644e26df050f60981e109a410
Jonas-Bergmann <bergmann.jonas31@gmail.com>
2022-09-22 22:34:12 +0200

    Add templates for views

--- ayto_solver/settings.py
+++ ayto_solver/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = "ayto_solver.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "frontend" / "templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
